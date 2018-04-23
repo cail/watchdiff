64c895fd0eb3d3cb37c2e852bb8a0ae612a6a31c
Igor Russkikh <igor.russkikh@aquantia.com>
2018-04-23 11:36:03 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # watchdiff
+
+watchdiff is a handy perl script to watch numeric differences in periodically running output of its command line.
+
+Its somewhat similar to `watch -d` but instead of just printing the output, it printsout all numbers "differences".
+
+Example is
+
+<pre>
+~/watchdiff 1 "ifconfig enp1s0f1 | grep packets"
+
+ifconfig enp1s0f1 | grep packets
+INCONSISTENT
+          RX packets:4066342529 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:14323464927 errors:0 dropped:0 overruns:0 carrier:0
+
+          RX packets:<<b>154,935</b>> errors:0 dropped:0 overruns:0 frame:0
+          TX packets:<<b>156,114</b>> errors:0 dropped:0 overruns:0 carrier:0
+
+          RX packets:<<b>154,668</b>> errors:0 dropped:0 overruns:0 frame:0
+          TX packets:<<b>156,154</b>> errors:0 dropped:0 overruns:0 carrier:0
+</pre>
+
+This essentially shows dynamic packet per second rates on the adapter.
+
+# Author
+
+https://github.com/cail/
+
+# License
+
+BSD
+
+# URL
+
+https://github.com/cail/watchdiff
---
+++ watchdiff.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+use Time::HiRes qw(usleep nanosleep);
+use Term::ANSIColor;
+
+$int = 1000*1000;
+
+$thsep = 1;
+
+if ($#ARGV == -1) {
+	print <<XX;
+Watching diff output in command numbers.
+Usage: watchdiff.sh [options] [interval] "shell command"
+	interval	in seconds (1s default). Could be used with ms suffix: 100ms
+	-r		do not use thousands separator
+XX
+	exit;
+}
+
+while ($#ARGV > 0) {
+	$sint=$ARGV[0];
+
+	if ($sint =~ /(\d+)ms/) {
+		$int=$1 * 1000;
+	} elsif ($sint =~ /(\d+)/) {
+		$int=$ARGV[0] * 1000 * 1000;
+	} elsif ($sint == "-r") {
+		$thsep = 0;
+	}
+	shift;
+}
+
+$cmd=$ARGV[0];
+
+while(true) {
+	$res = qx($cmd);
+	#print "$res\n";
+	
+	my @pmatches = $pres =~ /\d+/g;
+	my @matches;
+	my @lmatchs;
+	my @lmatche;
+	while($res =~ /(\d+)/g) {
+		push @matches, $1;
+		push @lmatchs, $-[0];
+		push @lmatche, $+[0];
+	}
+	#print "@matches\n";
+	#print "@pmatches\n";
+
+	if ($#matches != $#pmatches) {
+		print "INCONSISTENT\n$res";
+		usleep $int;
+		$pres = $res;
+		next;
+	}
+	
+	$dres = $res;
+	$notzero = 0;
+	for(my $i = $#matches; $i >= 0; $i--)
+	{
+		my $di = $matches[$i] - $pmatches[$i];
+		if ($di != 0) {
+			$notzero = 1;
+		}
+
+		my $s = $lmatchs[$i];
+		my $e = $lmatche[$i];
+		#print "se: $s $e\n";
+		
+		if ($di != 0) {
+			if ($thsep) {
+				$di = reverse join ',', unpack '(A3)*', reverse $di;
+			}
+			substr($dres, $s, $e-$s) = "<" . color("red") . "$di" . color("reset") . ">";
+		}
+		# $dres = $dres .  . substr($res, $s, $e-$s);
+	}
+	if ($notzero) {
+		print "$dres";
+	}else{
+		print "no diff.. ";
+	}
+
+	usleep($int);
+	$pres = $res;
+}
